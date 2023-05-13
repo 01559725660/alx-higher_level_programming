1aa2202492ac0a4d452ee22f31d962fc62c1490c
01559725660 <batoolkhaled@gmail.com>
2023-05-13 09:47:11 +0000

    k

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#Python - Data Structures: Lists, Tuples
+#5-no_c.py
+
+def no_c(my_string):
+    listofchars = list(my_string)
+    for chars in listofchars:
+        if char == 'c' or char == 'c':
+        listofchars.remove(char)
+        return("".join(listofchars))
