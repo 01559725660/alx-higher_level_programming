d368372b268c9e967716eb80dd1c65a89fea3370
01559725660 <batoolkhaled@gmail.com>
2023-05-14 19:12:14 +0000

    k

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#Python - Data Structures: Lists, Tuples
+#7-add_tuple.py
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
