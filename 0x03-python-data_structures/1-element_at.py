3b5afb8a7c32f426e6b8b2561a3716be33e11920
01559725660 <batoolkhaled@gmail.com>
2023-05-15 10:34:03 +0000

    k

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-#Python - Data Structures: Lists, Tuples
-#1-element_at.py
-
 def element_at(my_list, idx):
     listlength = len(my_list) - 1 
     if (idx < 0 or idx > listlength):
