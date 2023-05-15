f1cd31e5042657a90cfb3d40da02f127297b3a79
01559725660 <batoolkhaled@gmail.com>
2023-05-15 10:30:57 +0000

    k

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-#Python - Data Structures: Lists, Tuples
-#4-new_in_list.py
-
 def new_in_list(my_list, idx, element):
     listlength = len(my_list) - 1
     if (idx < 0 or idx > listlength):
