4ea960cd8af626de94ec9a2d47cca15e98587849
01559725660 <batoolkhaled@gmail.com>
2023-05-15 09:24:01 +0000

    k

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,7 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
+    print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
     for row in matrix:
         new_matrix.append([number ** 2 for number in row])
     return new_matrix
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,14 @@
 #Python - Data Structures: Lists, Tuples
 #7-add_tuple.py
 
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+def add_tuple(tuple_a=(), tuple_b=()):
+    if (len(tuple_a) == 0):
+        tuple_a = (0, 0)
+    elif (len(tuple_a) == 1):
+        tuple_a = (tuple_a[0], 0)
+    if (len(tuple_b) == 0):
+        tuple_b = (0, 0)
+    elif (len(tuple_b) == 1):
+        tuple_b = (tuple_b[0], 0)
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
