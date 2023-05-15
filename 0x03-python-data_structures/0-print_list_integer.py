1dd57022a61e5c332ad6f508f3d4ee070d6117e9
01559725660 <batoolkhaled@gmail.com>
2023-05-15 10:35:16 +0000

    k

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-#Python - Data Structures: Lists, Tuples
-#0-print_list_integer.py
-
 def print_list_integer(my_list=[]):
       for i in my_list:
           print('{:d}'.format(i))    
