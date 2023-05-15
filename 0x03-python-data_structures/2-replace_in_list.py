912b70f1697c7feee44ce269ae33da6564a09f56
01559725660 <batoolkhaled@gmail.com>
2023-05-15 10:33:05 +0000

    k

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-#Python - Data Structures: Lists, Tuples
-#2-replace_in_list.py
-
 def replace_in_list(my_list, idx, element):
    listlength = len(my_list) - 1
    if (idx < 0 or idx > listlength):
