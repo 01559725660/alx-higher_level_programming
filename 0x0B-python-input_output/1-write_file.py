6f51b8307c9cc702f1ee855a6adf4bab8a141673
01559725660 <batoolkhaled02@gmail.com>
2023-07-18 10:54:06 +0000

    p

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-
-
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="UTF-8") as f:
-        return f.write(text)
+   """writes a string to a text file
+   Args:
+       filename(str): filename
+       text(str): text to write
+   Returns:
+       number of characters written
+   """
+   with open(filename, "w", encoding="UTF-8") as f:
+       return f.write(text)
