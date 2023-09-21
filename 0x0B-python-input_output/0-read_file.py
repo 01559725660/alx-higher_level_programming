b9e70deb8b7b8ae9865645b5accafd24cfc64995
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-09-21 16:04:29 +0000

    p

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """read file contents
-    Args:
-        filename(str): filename
-    """
-    with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, "r", encoding="UTF8") as f:
+        print(f.read())
+read_file("my_file_0.txt")
