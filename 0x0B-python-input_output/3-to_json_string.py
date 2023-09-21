aa237498d3d3d889edce4061e099007c7d12df16
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-09-21 17:32:01 +0000

    p

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+""" Module that contains a function that returns the JSON
+representation of an object
+"""
 import json
-"""JavaScript Object Notation"""
 
 
 def to_json_string(my_obj):
-    """find JSON representation of an object (string)
+    """ Function that returns the JSON representation of an object
+
     Args:
-        my_obj(obj): object
-    Returns:
-        JSON representation of object
+        my_obj: object
+
+    Raises:
+        Exception: when the object can't be encoded
+
     """
     return json.dumps(my_obj)
